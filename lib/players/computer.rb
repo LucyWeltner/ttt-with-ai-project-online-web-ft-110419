9d90c40dbf3d539d304568064802a8ce6c548f73
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-01 17:04:51 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,12 +2,15 @@ module Players
   class Computer < Player 
     def move(board)
       spots = []
+      p board.cells
       board.cells.each_with_index do |spot, index|
         if spot == " "
           spots << index.to_s
         end 
       end
+      p spots
       spots.sample
+      rand(1..9).to_s
     end
   end 
 end 
