5ab493d6c47eeba511944b2c0474fd3f5d4c8f0b
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-01 17:03:17 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,13 @@
 module Players 
   class Computer < Player 
     def move(board)
-      rand(1..9).to_s
+      spots = []
+      board.cells.each_with_index do |spot, index|
+        if spot == " "
+          spots << index.to_s
+        end 
+      end
+      spots.sample
     end
   end 
 end 
